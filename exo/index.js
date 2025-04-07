508ad5b67c885201277f1ea4a29cba3fa63ba5ad
KUCA Brice <kuca.b@sfeir.com>
2025-04-07 10:30:20 +0200

    feat: add filterby

--- exo/index.js
+++ exo/index.js
@@ -14,3 +14,21 @@ function verifyToken(token) {
 }
 
 console.log(verifyToken(generateToken({ name: "KUCA Brice", year: 22 })));
+
+const users = [
+    { name: "Brice", year: 22 },
+    { name: "Alice", year: 20},
+    { name: "Bob", year: 30 },
+    { name: "Charlie", year: 17},
+    { name: 'John', year: 25 },
+    { name: 'Jane', year: 28 },
+]
+
+//Implémentez une fonction qui prend en entrée ce tableau et un critère de filtrage.
+//La fonction doit retourner un nouveau tableau contenant uniquement les éléments qui répondent au critère spécifié.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+function filterBy(users, name, maxYear, minYear) {
+    return users.filter((user) => user.name.includes(name) && user.year <= maxYear && user.year >= minYear);
+}
+
+console.log(filterBy(users, "B", 24, 20));
